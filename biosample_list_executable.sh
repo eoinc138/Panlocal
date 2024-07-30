d7fbe965da077ef037b1d7c0b1e5dd8f52de39d8
SheridanPO-Lab <136815173+SheridanPO-Lab@users.noreply.github.com>
2024-07-30 16:00:30 +0100

    Update biosample_list_executable.sh

--- biosample_list_executable.sh
+++ biosample_list_executable.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #Read the representative proteins file and only extract the FASTA headers.
-bzcat progenomes3.proteins.representatives.fasta.bz2 | pv | bzgrep '^>' > FASTA_HEADERS.txt>
+bzcat progenomes3.proteins.representatives.fasta.bz2 | bzgrep '^>' > FASTA_HEADERS.txt
 
 #Remove everything after the gene ID in the FASTA headers.
-sed 's/.GCA*//' FASTA_HEADERS.txt 
+sed -i 's/.GCA.*//g' FASTA_HEADERS.txt 
 
 #Sort and remove all duplicates from the Biosample text file.
 sort FASTA_HEADERS.txt | uniq > Biosample_IDs_filtered.txt
