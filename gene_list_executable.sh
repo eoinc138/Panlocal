a2cb1dc3c13683ac5447b0e4068cbc8c155a9dac
SheridanPO-Lab <136815173+SheridanPO-Lab@users.noreply.github.com>
2024-07-30 16:02:16 +0100

    Update gene_list_executable.sh

--- gene_list_executable.sh
+++ gene_list_executable.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #Read the representative gene file and only extract the FASTA headers.
-bzcat progenomes3.genes.representatives.fasta.bz2 | pv | bzgrep '^>' > FASTA_HEADERS.txt
+bzcat progenomes3.genes.representatives.fasta.bz2 | bzgrep '^>' > FASTA_HEADERS.txt
 
 #Remove everything after the < character in the FASTA headers.
-sed 's/<*//' FASTA_HEADERS.txt 
+sed -i 's/<*//' FASTA_HEADERS.txt 
 
 #Remove the first character and everything the the < onwards from each entry
 cut -c2- FASTA_HEADERS.TXT | sed 's/<.*//' > GeneIDS.txt
